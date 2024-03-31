6eb8c11ab0aaa43d206b06452689f0e91b8dc208
GENYST\laura <nunez.laura.e@protonmail.com>
2024-03-31 22:37:39 +0200

    Slight changes to About page

--- vue-to-do/src/views/AboutView.vue
+++ vue-to-do/src/views/AboutView.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>This is a CRUD To-Do tracking application created in Vue 3 for learning purposes.</h2>
   </div>
 </template>
 
 <style>
+.about {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 80px;
+  margin-left: 5%;
+}
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
     display: flex;
-    align-items: center;
+    margin-top: 80px;
+  margin-left: 5%;
   }
 }
 </style>
